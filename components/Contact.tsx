cad4131fd7f8e53aaa2e086fa665560f6b7bd25e
Tautis <69822476+Tautis@users.noreply.github.com>
2025-03-20 11:28:35 +0200

    ma

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import CustomButton, { ButtonColor, ButtonSize } from "./CustomButton";
 import SmokeEffect from "@/components/WebGLCanvas";
